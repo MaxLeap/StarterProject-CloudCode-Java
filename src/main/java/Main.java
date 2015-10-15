6bf81b3e8bc29dcca9dce16cc5f99885505dbd3a
yangyong <yyang@ilegendsoft.com>
2015-10-15 17:38:49 +0800

    rename Class name from LAS to ML

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import com.maxleap.code.*;
 import com.maxleap.code.impl.GlobalConfig;
-import com.maxleap.code.impl.LASResponse;
+import com.maxleap.code.impl.MLResponse;
 import com.maxleap.code.impl.LoaderBase;
 
 /**
@@ -12,20 +12,20 @@ public class Main extends LoaderBase implements Loader{
   @Override
   public void main(GlobalConfig globalConfig) {
     //定义一个简单的function:返回输入数据
-    defineFunction("hello", new LASHandler<Request, Response>() {
+    defineFunction("hello", new MLHandler<Request, Response>() {
       @Override
       public Response handle(Request request) {
-        Response<String> response = new LASResponse<>(String.class);
+        Response<String> response = new MLResponse<>(String.class);
         response.setResult(request.parameter(String.class));
         return response;
       }
     });
 
     //定义一个简单的job
-    defineJob("helloJob", new LASHandler<Request, Response>() {
+    defineJob("helloJob", new MLHandler<Request, Response>() {
       @Override
       public Response handle(Request request) {
-        Response response = new LASResponse(String.class);
+        Response response = new MLResponse(String.class);
         response.setResult("hello job");
         return response;
       }
